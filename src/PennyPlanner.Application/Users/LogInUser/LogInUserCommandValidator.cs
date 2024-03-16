d9410271800ce5791065659d5ff112bcb0f741a0
DBrdak <brdak02dominik@gmail.com>
2024-03-16 17:09:49 +0100

    LoginCommandValidator added

---
+++ src/PennyPlanner.Application/Users/LogInUser/LogInUserCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using PennyPlanner.Application.Settings.ValidationSettings;
+
+namespace PennyPlanner.Application.Users.LogInUser
+{
+    public sealed class LogInUserCommandValidator : AbstractValidator<LogInUserCommand>
+    {
+        public LogInUserCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .Matches(UserValidationSettings.EmailPattern)
+                .WithMessage("Invalid email");
+        }
+    }
+}
